63d53b124ca5c84fa5f94d78b5ecb89721ec0319
wizdo-johan <soldierheart@hotmail.com>
2021-05-24 14:24:36 +0200

    following tutorial

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -3,7 +3,21 @@ import Post from './Post'
 import Banner from '../Banner'
 const Posts = ({ posts, title }) => {
   return (
-    <h4>posts component</h4>
+    <section className="posts">
+      <h3 className="posts-title">{title}</h3>
+      <div className="posts-center">
+        {/* post columns */}
+        <article>
+          {posts.map(post => {
+            return <Post key={post.id} {...post} />
+          })}
+        </article>
+        {/* banner columns */}
+        <article>
+          <Banner />
+        </article>
+      </div>
+    </section>
   )
 }
 
